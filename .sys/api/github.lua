12159f9e2173cdb95536ae8389421928049d2792
FFGFlash <26831072+FFGFlash@users.noreply.github.com>
2023-11-06 20:51:48 -0600

    Added Github and JSON apis

---
+++ .sys/api/github.lua
@@ -0,0 +1,76 @@
+local github = api(2, {
+  {
+    type = 'choice',
+    options = {
+      download = {}
+    }
+  }
+})
+
+github.PROXY_URL = 'https://nekos-api-dev-sprp.2.us-1.fl0.io/github/'
+
+function github:getRepo(user, repo)
+  if repo == nil or user == nil then return false, 'User and repo are required.' end
+  local res = http.get(self.PROXY_URL..user..'/'..repo)
+  if not res then return false, "Can't resolve manifest url." end
+  return json:fromStream(res)
+end
+
+function github:download(user, repo, dpath, rpath, branch, extract)
+  if repo == nil or user == nil then return false, 'User and repo are required.' end
+  dpath, rpath, branch, extract = dpath or '/downloads/', rpath or '', branch or 'main', extract or false
+
+  local function downloadManager(path, files, dirs)
+    files, dirs = files or {}, dirs or {}
+    local ftype, fpath, fname = {}, {}, {}
+    local res = http.get(self.PROXY_URL..user..'/'..repo..'/content?branch='..branch..'&path='..path)
+    if not res then return false, "Can't resolve download url." end
+    res = res.readAll()
+    if res ~= nil then
+      for str in res:gmatch('"type":"(%w+)"') do table.insert(ftype, str) end
+      for str in res:gmatch('"path":"([^\"]+)"') do table.insert(fpath, str) end
+      for str in res:gmatch('"name":"([^\"]+)"') do table.insert(fname, str) end
+    end
+    for i, data in pairs(ftype) do
+      local path = dpath..'/'
+      if not extract then path = path..repo..'/' end
+      if data == 'file' then
+        local cpath = http.get(self.PROXY_URL..user..'/'..repo..'/'..branch..'?path='..fpath[i])
+        if cpath == nil then fpath[i] = fpath[i]..'/'..fname[i] end
+        path = path..fpath[i]
+        if not files[path] then files[path] = { self.PROXY_URL..user..'/'..repo..'/'..branch..'?path='..fpath[i], fname[i] } end
+      elseif data == 'dir' then
+        path = path..fpath[i]
+        if not dirs[path] then
+          dirs[path] = { self.PROXY_URL..user..'/'..repo..'/'..branch..'?path='..fpath[i], fname[i]}
+          downloadManager(fpath[i], files, dirs)
+        end
+      end
+    end
+    return { files = files, dirs = dirs }
+  end
+
+  local function downloadFile(path, url, name)
+    local dpath = path:gmatch('([%w%_%.% %-%+%,%;%:%*%#%=%/]+)/'..name..'$')()
+    if dpath ~= nil and not fs.isDir(dpath) then fs.makeDir(dpath) end
+    local content = http.get(url)
+    local file = fs.open(path,"w")
+    file.write(content.readAll())
+    file.close()
+  end
+
+  local res, err = downloadManager(rpath)
+  if not res then return res, err end
+  for path, data in pairs(res.files) do downloadFile(path, table.unpack(data)) end
+
+  local meta = self:getRepo(user, repo)
+  local mpath = dpath..'/'
+  if not extract then mpath = mpath..repo..'/' end
+  local mfile = fs.open(mpath..'.manifest', 'w')
+  mfile.write(json:stringify(meta, true))
+  mfile.close()
+
+  return true
+end
+
+return github:call(...)
---
+++ .sys/api/json.lua
@@ -0,0 +1,163 @@
+local json = api(0)
+
+function json:init()
+  self.whites = { ['\n'] = true, ['\r'] = true, ['\t'] = true, [' '] = true, [','] = true, [':'] = true }
+  self.numChars = { ['e'] = true, ['E'] = true, ['+'] = true, ['-'] = true, ['.'] = true }
+  self.escaped = { ['\n'] = '\\n', ['\r'] = '\\r', ['\t'] = '\\t', ['\b'] = '\\b', ['\f'] = '\\f', ['"'] = '\\"', ['\\'] = '\\\\' }
+  self.iescaped = {}
+  for k,v in pairs(self.escaped) do self.iescaped[v] = k
+  end
+end
+
+function json:parse(str)
+  local function removeWhite(s)
+    while self.whites[s:sub(1,1)] do s = s:sub(2)
+    end
+    return s
+  end
+
+  local function parseBoolean(s)
+    local v = s:sub(1,4) == 'true'
+    return v, removeWhite(s:sub(v and 5 or 6))
+  end
+
+  local function parseNull(s)
+    return nil, removeWhite(s:sub(5))
+  end
+
+  local function parseNumber(s)
+    local i = 1
+    while self.numChars[s:sub(i,i)] or tonumber(s:sub(i,i)) do i = i + 1
+    end
+    local v = tonumber(s:sub(1, i - 1))
+    return v, removeWhite(s:sub(i))
+  end
+
+  local function parseString(s)
+    s = s:sub(2)
+    local v = ''
+    while s:sub(1,1) ~= '"' do
+      local n = s:sub(1, 1)
+      s = s:sub(2)
+      assert(n ~=  '\n', 'Unclosed string')
+      if n == '\\' then
+        local e = s:sub(1, 1)
+        s = s:sub(2)
+        n = assert(self.iescaped[n..e], "Invalid escaped character")
+      end
+      v = v..n
+    end
+    return v, removeWhite(s:sub(2))
+  end
+
+  local function parseArray(s)
+    s = removeWhite(s:sub(2))
+    local a, i = {}, 1
+    while s:sub(1, 1) ~= ']' do
+      local v = nil
+      v, s = parseValue(s)
+      a[i] = v
+      i = i + 1
+      s = removeWhite(s)
+    end
+    return a, removeWhite(s:sub(2))
+  end
+
+  local function parseMember(s)
+    local k, v = nil, nil
+    k, s = parseValue(s)
+    v, s = parseValue(s)
+    return k, v, s
+  end
+
+  local function parseObject(s)
+    s = removeWhite(s:sub(2))
+    local t = {}
+    while s:sub(1,1) ~= '}' do
+      local k, v = nil, nil
+      k, v, s = parseMember(s)
+      t[k] = v
+      s = removeWhite(s)
+    end
+    return t, removeWhite(s:sub(2))
+  end
+
+  local function parseValue(s)
+    local f = s:sub(1,1)
+    if f == "{" then return parseObject(s)
+    elseif f == '[' then return parseArray(s)
+    elseif tonumber(f) ~= nil or self.numChars[f] then return parseNumber(s)
+    elseif s:sub(1, 4) == 'true' or s:sub(1, 5) == 'false' then return parseBoolean(s)
+    elseif f == '"' then return parseString(s)
+    elseif f == 'null' then return parseNull(s)
+    end
+    return nil
+  end
+
+  return parseValue(removeWhite(str))
+end
+
+function json:fromStream(stream)
+  return self:parse(stream.readAll())
+end
+
+function json:fromFile(file)
+  file = fs.open(file, 'r')
+  local ret = self:fromStream(file)
+  file.close()
+  return ret
+end
+
+function json:stringify(json, pretty, u, o)
+  local str = ''
+  pretty, u, o = pretty or false, u or 0, o or {}
+  
+  local function tab(i) str = str..('\t'):rep(u)..i
+  end
+
+  local function encoding(t, b, c, i, l)
+    str = str..b
+    if pretty then
+      str = str..'\n'
+      u = u + 1
+    end
+    for k,v in i(t) do
+      tab('')
+      l(k, v)
+      str = str..','
+      if pretty then str = str..'\n'
+      end
+    end
+    if pretty then u = u - 1
+    end
+    if str:sub(-2) == ',\n' then str = str:sub(1, -3)..'\n'
+    elseif str:sub(-1) == ',' then str = str:sub(1, -2)
+    end
+    tab(c)
+  end
+
+  if type(json) == 'table' then
+    assert(not o[json], 'Cannot handle cyclic tables.')
+    o[json] = true
+    if table.isArray(json) then
+      encoding(json, '[', ']', ipairs, function(k, v)
+        str = str..self:stringify(v, pretty, u, o)
+      end)
+    else
+      encoding(json, '{', '}', pairs, function(k, v)
+        assert(type(k) == 'string', 'JSON Object keys must be strings.', 2)
+        str = str..self:stringify(k, pretty, u, o)
+        str = str..(pretty and ': ' or ':')..self:stringify(v, pretty, u, o)
+      end)
+    end
+  elseif type(json) == 'string' then
+    str = '"'..json:gsub('[%c"\\]', self.escaped)..'"'
+  elseif type(json) == 'number' or type(json) == 'boolean' then
+    str = tostring(json)
+  else
+    error('JSON only supports arrays, objects, numbers, booleans and strings')
+  end
+  return str
+end
+
+return json:call(...)
