aaa44f11aa38824ab1a5d907ed1449f371be7300
FFGFlash <26831072+FFGFlash@users.noreply.github.com>
2023-11-06 22:22:07 -0600

    add help api

---
+++ .sys/api/help.lua
@@ -0,0 +1,23 @@
+local help = api(0, {
+  { type = 'api', name = 'program' }
+})
+
+function help:execute(args, program, ...)
+  if not program then
+    print('Welcome to NekOS')
+    for name, program in pairs(api.List) do
+      if program.printUsage then program:printUsage()
+      end
+    end
+  else
+    program = api.List[program]
+    if not program then
+      print('Unknown prgoram')
+      self:execute(args)
+    else
+      program:printUsage()
+    end
+  end
+end
+
+return help:call(...)
